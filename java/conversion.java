773e806aa04f03dbef83469209c928cf9c4b8eb6
Jayasri seenu <e21it023@shanmugha.edu.in>
2025-04-02 11:05:29 +0530

    Add files via upload

---
+++ java/conversion.java
@@ -0,0 +1,7 @@
+public class conversion {
+    public static void main(String[] args) {
+        int n=10;
+        String binary=Integer.toBinaryString(n);
+        System.out.println(binary);
+    }
+}
