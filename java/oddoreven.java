02c62e3907007ae4ae2a8fb3a8cac9a954f15b93
Jayasri seenu <e21it023@shanmugha.edu.in>
2025-04-03 13:03:34 +0530

    Add files via upload

---
+++ java/oddoreven.java
@@ -0,0 +1,11 @@
+public class oddoreven {
+    public static void main(String[] args) {
+        int num=12;
+        if(num %2==0){
+            System.out.println(" the numbers in even");
+        }
+        else{
+            System.out.println(" the numbers is odd. ");
+        }
+    }
+}
