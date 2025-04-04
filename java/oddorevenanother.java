7af62ea5d4aced77c2bd778f8c04d58b53b5e684
Jayasri seenu <e21it023@shanmugha.edu.in>
2025-04-04 14:29:45 +0530

    Add files via upload

---
+++ java/oddorevenanother.java
@@ -0,0 +1,12 @@
+public class oddorevenanother {
+//odd or even with out using n%2==0
+    public static void main(String[] args) {
+        int num=12;
+        if((num &1)==0){
+            System.out.println(" the given number is even number,");
+        }
+        else{
+            System.out.println("the given numbers is odd number.");
+        }
+    }
+}
