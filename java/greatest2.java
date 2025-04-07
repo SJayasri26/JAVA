4962fef48735118e9897ca59fa1bd279c39111dc
Jayasri seenu <e21it023@shanmugha.edu.in>
2025-04-07 14:45:50 +0530

    Add files via upload

---
+++ java/greatest2.java
@@ -0,0 +1,13 @@
+public class greatest2 {
+    public static void main(String[] args) {
+        int n1=38;
+        int n2=48;
+        int greatest;
+        if(n1>=n2){
+            greatest=n1;
+        }else{
+            greatest=n2;
+        }
+        System.out.println("The greatest value is : "+greatest);
+    }
+}
