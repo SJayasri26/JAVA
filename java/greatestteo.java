2236888b702a8de032616d980cc8acf86687d08f
Jayasri seenu <e21it023@shanmugha.edu.in>
2025-04-07 14:53:35 +0530

    Delete java/greatestteo.java

--- java/greatestteo.java
+++
@@ -1,8 +0,0 @@
-public class greatestteo {
-    public static void main(String[] args) {
-        int n1=30;
-        int n2=40;
-        int greatest=(n1+n2+Math.abs(n1-n2))/2;
-        System.out.println(" the greatest number is : "+greatest);
-    }
-}
