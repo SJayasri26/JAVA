823569670d898162bfd8c280c4c3fcd3d7de6b5b
Jayasri seenu <e21it023@shanmugha.edu.in>
2025-04-08 18:46:59 +0530

    Add files via upload

---
+++ java/sumnum.java
@@ -0,0 +1,7 @@
+public class sumnum {
+    public static void main(String[] args) {
+        int n =5;
+        int sum=(n*(n+1))/2;
+        System.out.println(" the sum of the first "+n+" numbers is : "+sum);
+    }
+}
