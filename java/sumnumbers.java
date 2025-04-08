f2eb34be2e22a53c166710de3a5de9362e2692f6
Jayasri seenu <e21it023@shanmugha.edu.in>
2025-04-08 18:48:37 +0530

    Add files via upload

---
+++ java/sumnumbers.java
@@ -0,0 +1,10 @@
+public class sumnumbers {
+    public static void main(String[] args) {
+        int n =5;
+        int sum =0;
+        for(int i=1;i<=n;i++){
+            sum +=i;
+        }
+        System.out.println("the sum of the first "+n+ " numbers is : "+sum);
+    }
+}
